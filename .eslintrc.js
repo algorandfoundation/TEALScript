61e888c991b6a7c4e661b007749ae5ab5f460def
Joe Polny <joepolny@gmail.com>
2022-12-26 19:45:20 -0500

    examples test with mocha

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'plugin:mocha/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -13,6 +14,7 @@ module.exports = {
   },
   plugins: [
     '@typescript-eslint',
+    'mocha',
   ],
   rules: {
   },
---
+++ tests/examples.ts
@@ -0,0 +1,31 @@
+/* eslint-disable mocha/max-top-level-suites */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+import * as fs from 'fs';
+import { expect } from 'chai';
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import Compiler from '../src/lib/compiler';
+
+['Master', 'Vault'].forEach((className) => {
+  describe(`ARC12 ${className}`, function () {
+    before(async function () {
+      const content = fs.readFileSync('examples/arc12/arc12.ts', 'utf-8');
+      this.compiler = new Compiler(content, className, 'examples/arc12/arc12.ts');
+      await this.compiler.compile();
+      await this.compiler.algodCompile();
+    });
+
+    it('Generates TEAL', function () {
+      expect(this.compiler.prettyTeal()).to.equal(fs.readFileSync(`examples/arc12/${className}.teal`, 'utf-8'));
+    });
+
+    it('Generates Sourcemap', function () {
+      expect(this.compiler.pcToLine).to.deep.equal(JSON.parse(fs.readFileSync(`examples/arc12/${className}.src_map.json`, 'utf-8')));
+    });
+
+    it('Generates ABI JSON', function () {
+      expect(this.compiler.abi).to.deep.equal(JSON.parse(fs.readFileSync(`examples/arc12/${className}.abi.json`, 'utf-8')));
+    });
+  });
+});
