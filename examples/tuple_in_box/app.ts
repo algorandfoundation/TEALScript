a55fe791b6a0419401ddd0a1a6e307c0fd860f2e
Joe Polny <joepolny@gmail.com>
2023-03-18 11:41:52 -0400

    use objects in box_in_tuple

--- examples/tuple_in_box/app.ts
+++ examples/tuple_in_box/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable object-shorthand */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-undef */
 
@@ -5,38 +6,34 @@ import { Contract } from '../../src/lib/index';
 
 // eslint-disable-next-line no-unused-vars
 class ContactsApp extends Contract {
-  // TOOD: NamedTuple
-  // NamedTuple will simply be a map of names in the compiler to indexes in the tuple
-  // 0 index = name
-  // 1 index = company
-  contacts = new BoxMap<Account, [string, string]>();
+  contacts = new BoxMap<Account, {name: string, company: string}>();
 
-  myContact = new GlobalReference<[string, string]>();
+  myContact = new GlobalReference<{name: string, company: string}>();
 
   @createApplication
   create(): void {}
 
   setMyContact(name: string, company: string): void {
-    const contact: [string, string] = [name, company];
+    const contact: {name: string, company: string} = { name: name, company: company };
 
     this.myContact.put(contact);
     this.contacts.put(this.txn.sender, contact);
   }
 
   addContact(name: string, company: string, address: Account): void {
-    const contact: [string, string] = [name, company];
+    const contact: {name: string, company: string} = { name: name, company: company };
     this.contacts.put(address, contact);
   }
 
   updateContactField(field: string, value: string, address: Account): void {
     if (field === 'name') {
-      this.contacts.get(address)[0] = value;
+      this.contacts.get(address).name = value;
     } else if (field === 'company') {
-      this.contacts.get(address)[1] = value;
+      this.contacts.get(address).company = value;
     } else err();
   }
 
   verifyContactName(name: string, address: Account): void {
-    assert(this.contacts.get(address)[0] === name);
+    assert(this.contacts.get(address).name === name);
   }
 }
