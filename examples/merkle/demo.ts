93dc55d1eb69fba3cb966dd6317fa3b175b68052
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-04-06 14:38:19 -0400

    adding thing to verify merkle correctness

--- examples/merkle/demo.ts
+++ examples/merkle/demo.ts
@@ -1,5 +1,23 @@
 import { sandbox, clients } from 'beaker-ts';
+import fs from 'fs';
 import { MerkleTree } from './artifacts/merkletree_client';
+
+async function getRecords(fname: string): Promise<Uint8Array[][]> {
+  const rawRecords: string[][] = fs.readFileSync(fname, 'utf8').split('\n').map((line) => line.trim().split(','));
+
+  const records: Uint8Array[][] = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const record of rawRecords) {
+    if (record.length === 4) {
+      const [data, ...path] = record;
+      records.push([Buffer.from(data, 'utf8'), ...path.map((p) => Buffer.from(p, 'hex'))]);
+    } else if (record.length === 5) {
+      const [oldData, newData, ...path] = record;
+      records.push([Buffer.from(oldData, 'utf8'), Buffer.from(newData, 'utf8'), ...path.map((p) => Buffer.from(p, 'hex'))]);
+    }
+  }
+  return records;
+}
 
 async function main() {
   const acct = (await sandbox.getAccounts()).pop()!;
@@ -19,6 +37,36 @@ async function main() {
   const expectedRoot = '80d1bf4dd6c1f75bba022337a3f0842078f5c2e7f3f59dfd33ccbb8e963367b2';
 
   console.log(`root match: ${root === expectedRoot}, root: ${root}, expected: ${expectedRoot}`);
+
+  const recordsToAppend = await getRecords('examples/merkle/append.csv');
+  // eslint-disable-next-line no-restricted-syntax
+  for (const record of recordsToAppend) {
+    const result = appClient.appendLeaf({ data: record[0], path: record.slice(1) });
+    console.log(result);
+  }
+
+  const recordsToVerify = await getRecords('examples/merkle/verify_appends.csv');
+  // eslint-disable-next-line no-restricted-syntax
+  for (const record of recordsToVerify) {
+    const result = appClient.verify({ data: record[0], path: record.slice(1) });
+    console.log(result);
+  }
+
+  const recordsToUpdate = await getRecords('examples/merkle/update.csv');
+  // eslint-disable-next-line no-restricted-syntax
+  for (const record of recordsToUpdate) {
+    const result = appClient.updateLeaft({
+      oldData: record[0], newData: record[1], path: record.slice(2),
+    });
+    console.log(result);
+  }
+
+  const recordsToVerifyAgain = await getRecords('examples/merkle/verify_updates.csv');
+  // eslint-disable-next-line no-restricted-syntax
+  for (const record of recordsToVerifyAgain) {
+    const result = appClient.verify({ data: record[0], path: record.slice(1) });
+    console.log(result);
+  }
 }
 
 main();
