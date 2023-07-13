15e5cc44a17380c41e8cf6d25b7b8dbf901acf61
Joe Polny <joepolny@gmail.com>
2023-07-13 17:51:57 +0200

    fix element access in memeber expression chain

--- tests/contracts/general.algo.ts
+++ tests/contracts/general.algo.ts
@@ -3,7 +3,6 @@ import { Contract } from '../../src/lib/index';
 // eslint-disable-next-line no-unused-vars
 class GeneralTest extends Contract {
   txnTypeEnum(): void {
-    const t = this.txnGroup[0];
-    assert(t.typeEnum === TransactionType.Payment);
+    assert(this.txnGroup[0].typeEnum === TransactionType.Payment);
   }
 }
