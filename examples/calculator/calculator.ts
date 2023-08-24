b0c30c8584548178c869b719fc7e534003470734
Joe Polny <joepolny@gmail.com>
2023-08-24 10:51:33 +0530

    chore: add/fix comments

--- examples/calculator/calculator.ts
+++ examples/calculator/calculator.ts
@@ -2,10 +2,24 @@
 /* eslint-disable class-methods-use-this */
 
 class Calculator {
+  /**
+   * Calculates the sum of two numbers
+   *
+   * @param a
+   * @param b
+   * @returns The sum of a and b
+   */
   private getSum(a: number, b: number): number {
     return a + b;
   }
 
+  /**
+   * Calculates the difference between two numbers
+   *
+   * @param a
+   * @param b
+   * @returns The difference between a and b.
+   */
   private getDifference(a: number, b: number): number {
     return a >= b ? a - b : b - a;
   }
@@ -13,9 +27,9 @@ class Calculator {
   /**
   * A method that takes two numbers and does either addition or subtraction
   *
-  * @param a - The first number
-  * @param b - The second number
-  * @param operation - The operation to perform. Can be either 'sum' or 'difference'
+  * @param a The first number
+  * @param b The second number
+  * @param operation The operation to perform. Can be either 'sum' or 'difference'
   *
   * @returns The result of the operation
   */
