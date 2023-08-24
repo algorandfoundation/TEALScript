b293c920f1924b47d79bbc5c0fdf35c4641837b2
Joe Polny <joepolny@gmail.com>
2023-08-24 10:34:56 +0530

    chore: add calculator.ts

---
+++ examples/calculator/calculator.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+/* eslint-disable class-methods-use-this */
+
+class Calculator {
+  private getSum(a: number, b: number): number {
+    return a + b;
+  }
+
+  private getDifference(a: number, b: number): number {
+    return a >= b ? a - b : b - a;
+  }
+
+  /**
+  * A method that takes two numbers and does either addition or subtraction
+  *
+  * @param a - The first number
+  * @param b - The second number
+  * @param operation - The operation to perform. Can be either 'sum' or 'difference'
+  *
+  * @returns The result of the operation
+  */
+  doMath(a: number, b: number, operation: string): number {
+    let result: number;
+
+    if (operation === 'sum') {
+      result = this.getSum(a, b);
+    } else if (operation === 'difference') {
+      result = this.getDifference(a, b);
+    } else throw Error('Invalid operation');
+
+    return result;
+  }
+}
+
+const calculator = new Calculator();
+
+console.log(calculator.doMath(5, 7, 'difference'));
