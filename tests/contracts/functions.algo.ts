ab8286ce7f16031c8443fe4b3042b34562823f09
Joe Polny <joepolny@gmail.com>
2024-09-14 10:03:18 -0400

    callPackageFunction

--- tests/contracts/functions.algo.ts
+++ tests/contracts/functions.algo.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { packageFunction } from 'tealscript_test_package';
 import { Contract } from '../../src/lib/index';
 import { externalFunction } from './functions-external.algo';
 
@@ -12,5 +14,9 @@ export class FunctionsTest extends Contract {
 
   callExternalFunction(a: uint64, b: uint64): uint64 {
     return externalFunction(a, b);
+  }
+
+  callPackageFunction(a: uint64, b: uint64): uint64 {
+    return packageFunction(a, b);
   }
 }
---
+++ tests/test_package/index.ts
@@ -0,0 +1,3 @@
+export function packageFunction(a: uint64, b: uint64): uint64 {
+  return a + b;
+}
